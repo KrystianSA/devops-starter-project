9e1deda68abd61bd5ed2b6f4ad94f3781f243df2
Krystian <krystiansa@protonmail.com>
2026-03-30 18:43:08 +0200

    add Terraform Azure infrastructure project

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "myfirstdevopsstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
